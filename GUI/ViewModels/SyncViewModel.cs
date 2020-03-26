a953b7f026830451b24df6b9262c4ae88f604d40
Marvin Gerdel <marvin.gerdel@web.de>
2020-03-26 18:43:36 +0100

    MVP bugs fixed

--- GUI/ViewModels/SyncViewModel.cs
+++ GUI/ViewModels/SyncViewModel.cs
@@ -2,7 +2,9 @@
 using DataStoring.Contracts.MovieModels;
 using FFmpegStandardWrapper.Abstract.Core;
 using FFmpegStandardWrapper.Abstract.Options;
+using NetStandard.Logger;
 using Ninject;
+using PanasonicSync.GUI.Messaging.Impl;
 using PanasonicSync.GUI.Tasks;
 using System;
 using System.Collections.Generic;
@@ -17,6 +19,7 @@ namespace PanasonicSync.GUI.ViewModels
 {
     public class SyncViewModel : ViewModelBase, IScreen
     {
+        private readonly ILogger _logger;
         private readonly IFFprobe _ffprobe;
         private readonly IFFmpeg _ffmpeg;
         private readonly IConversionOptions _conversionOptions;
@@ -45,7 +48,19 @@ namespace PanasonicSync.GUI.ViewModels
 
         public SyncViewModel(IEnumerable<IMovieFile> movies)
         {
+            _logger = _standardKernel.Get<ILoggerFactory>().CreateFileLogger();
             Movies = new BindableCollection<IMovieFile>(movies);
+
+            //IList<Tuple<string, bool>> steps = new List<Tuple<string, bool>>();
+
+            //foreach(var movie in Movies)
+            //{
+            //    steps.Add(movie.)
+            //}
+
+            //SendMessage(new SetProgressControlMessage(new ProgressbarViewModel
+            //{
+            //}));
 
             var downloadScheduler = new LimitedConcurrencyLevelTaskScheduler(1);
             var conversionScheduler = new LimitedConcurrencyLevelTaskScheduler(1);
@@ -92,27 +107,32 @@ namespace PanasonicSync.GUI.ViewModels
             try
             {
                 movie.FilePath = $@"tmp\{movie.Title}.tts";
+                _logger.Debug($"Download started for [{movie.FileLink}]-[{movie.FilePath}]");
                 using (var client = new WebClient())
                 {
                     client.DownloadFile(movie.FileLink, movie.FilePath);
                 }
+                _logger.Debug($"Download finished for [{movie.FileLink}]-[{movie.FilePath}]");
 
                 Task task = _conversionFactory.StartNew(() => Convert(movie), _cts.Token);
                 _conversionTasks.Add(task);
             }
             catch (Exception ex)
             {
-
+                _logger.Error($"Download failed for [{movie.FileLink}]-[{movie.FilePath}]", ex);
             }
         }
 
         private void Convert(IMovieFile movie)
         {
+            var newPath = Path.ChangeExtension(movie.FilePath, ".mp4");
             try
             {
-                var info = _ffprobe.GetDetailedMovieInformation(movie.FilePath);
-                var newPath = Path.ChangeExtension(movie.FilePath, ".mp4");
-                _ffmpeg.Convert(info, newPath, _conversionOptions);
+                _logger.Debug($"Conversion started for [{movie.FilePath}]-[{newPath}]");
+                _ffmpeg.Convert(movie.FilePath, newPath, _conversionOptions);
+                _logger.Debug($"Conversion finished for [{movie.FilePath}]-[{newPath}]");
+                File.Delete(movie.FilePath);
+                _logger.Debug($"Deleted [{movie.FilePath}]");
                 movie.FilePath = newPath;
 
                 Task task = _transferFactory.StartNew(() => Transfer(movie), _cts.Token);
@@ -120,7 +140,7 @@ namespace PanasonicSync.GUI.ViewModels
             }
             catch (Exception ex)
             {
-
+                _logger.Error(ex, $"Conversion / Deletion failed for [{movie.FilePath}]-[{newPath}]");
             }
         }
 
@@ -128,7 +148,16 @@ namespace PanasonicSync.GUI.ViewModels
         {
             var fileName = Path.GetFileName(movie.FilePath);
             var targetPath = Path.Combine(@"N:\Movies\ALLE_RECS", fileName);
-            File.Move(movie.FilePath, fileName);
+            try
+            {
+                _logger.Debug($"Move started for [{movie.FilePath}]-[{targetPath}]");
+                File.Move(movie.FilePath, targetPath);
+                _logger.Debug($"Move finished for [{movie.FilePath}]-[{targetPath}]");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"Move failed for [{movie.FilePath}]-[{targetPath}]");
+            }
         }
     }
 }
